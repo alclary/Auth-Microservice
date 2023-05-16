932ff07975cd3ce6312867796e4643afe3d9c198
LoganC <alclary@posteo.net>
2023-05-15 20:42:48 -0500

    SQL integration

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Auth-Microservice
-This application represents a small [ZeroMQ-based](https://zeromq.org/) authentication microservice. This tiny auth server will listen on a port-bound socket for incoming packets containing simple JSON objects, in the format `{"username": [username], "password": [password]}`. The serialized JSON object received by the server will be validated against a [JSON schema](https://json-schema.org/understanding-json-schema/about.html) definition of that format. 
 
-Assuming the provided object produces no validation error, the given case-sensitive username will be checked against the record of the service's connected DB. If a username match is found, the case-sensitive password will be checked against the same username record. If the match of both keys is successful, a simple string message response of `valid` will be sent by the server. Should either of those matches fail, or the aforementioned data validation fail, a message response of `invalid` will be sent by the server. Please see the `test_client.py` file in the repo for an example of the data validation in action. 
+This application represents a small [ZeroMQ-based](https://zeromq.org/) authentication microservice. This tiny auth server will listen on a port-bound socket for incoming packets containing simple JSON objects, in the format `{"username": [username], "password": [password]}`. The serialized JSON object received by the server will be validated against a [JSON schema](https://json-schema.org/understanding-json-schema/about.html) definition of that format.
 
-At this time, and as requested by my CS361 partner, the microservice supports a static JSON database, provided as a single file containing an array of JSON objects describing users. See the `static_db.json` file in this repo for an example of the expected format. User objects may contain any number of keys, but they *must* contain `username` and `password` keys. 
+Assuming the provided object produces no validation error, the given case-sensitive username will be checked against the record of the service's connected DB. If a username match is found, the case-sensitive password will be checked against the same username record. If the match of both keys is successful, a simple string message response of `valid` will be sent by the server. Should either of those matches fail, or the aforementioned data validation fail, a message response of `invalid` will be sent by the server. Please see the `test_client.py` file in the repo for an example of the data validation in action.
 
-The microservice has been built in such a way that it will easily support SQL DB integration in the future. In fact, the default option for the CLI is reserved for SQL DB use. Most likely, SQL server connection details will be provided via environment file or vars. As an alternative----and the focus of the current implementation----a static JSON db file can be provided via CLI flags `-j` or `--json`.
+The microservice supports querying against a live mySQL database or a static JSON database---- the latter provided as a single file containing an array of JSON objects describing users. See the `static_db.json` file in this repo for an example of the expected format. User objects in either type of database may contain any number of attributes/keys, but they _must_ contain `username` and `password` attributes/keys.
+
+By default, the microservice python file does not require any addition CLI arguments and will intend to use a live SQL database. However, it will expect a `.env` file to be located in the same directory as the `auth_server.py` file, containing the following variables, one to a line, in order to connect to the SQL DB: `MYSQL_HOST=, MYSQL_USER=, MYSQL_PASSWORD=, MYSQL_DATABASE=`. DB details can be entered directly after the equal sign, without quotes or wrapped in quotes. Spaces are permitted before and after the equal sign. The `.env_example` file contains an example of the requires vars, without assignments.
+
+Alternatively, to utilize a static JSON db, a JSON file can be provided via CLI flags `-j` or `--json`.
 
 ## Dependencies
-To get started, ensure that the `pyzmq` and `jsonschema` packages are installed to your environment or system:
-```pip install pyzmq jsonschema```
+
+To get started, ensure that the `pyzmq`,`mysql`,`mysql-connector-python`, `python-dotenv`, and `jsonschema`, packages are installed to your environment or system:
+`pip install pyzmq, mysql, mysql-connector-python, python-dotenv, jsonschema`
 
 ## Configuration
 
@@ -18,13 +22,13 @@ If you would like the microservice to listen on a specific port, please edit the
 ## Calling the service
 
 The authentication microservice is intended to be started via the CLI. To start the service:
-```python auth_server.py```
+`python auth_server.py`
 
 For an elaboration of currently available command line arguments:
-```python auth_server.py -h```
+`python auth_server.py -h`
 
-To utilize the service with a static JSON DB file: 
-```python auth_server.py --json db_filename.json```
+To utilize the service with a static JSON DB file:
+`python auth_server.py --json db_filename.json`
 
 ## Sending and Receiving Data
 
@@ -34,7 +38,8 @@ For example, a real packet might contain:
 `{"username": "julioJones", "password": "falcons"}`
 
 In the client, standard the dependencies named above should be imported, and then a ZeroMQ socket readied to make calls to the server. For example:
-```python 
+
+```python
 import zmq
 import json
 
@@ -51,9 +56,9 @@ reply = socket.recv_string()
 print(reply)
 ```
 
-Notably, it is not mandatory to utilize ZeroMQ's send_string() and recv_string(), but it does simplify encoding and serialization within the code. Please see the [ZMQ Socket Class](https://pyzmq.readthedocs.io/en/latest/api/zmq.html#zmq.Socket) documentation for more details (i.e. see recv_ and send_ class methods). For example, to avoid serialization of JSON objects (i.e. stringifying), an approach like the following could be used:
+Notably, it is not mandatory to utilize ZeroMQ's send_string() and recv_string(), but it does simplify encoding and serialization within the code. Please see the [ZMQ Socket Class](https://pyzmq.readthedocs.io/en/latest/api/zmq.html#zmq.Socket) documentation for more details (i.e. see recv\* and send\_ class methods). For example, to avoid serialization of JSON objects (i.e. stringifying), an approach like the following could be used:
 
-```python 
+```python
 import zmq
 import json
 
--- auth_server.py
+++ auth_server.py
@@ -1,10 +1,13 @@
+# pip install pyzmq, mysql, mysql-connector-python, python-dotenv, jsonschema
+import os
 import argparse
 import json
 import signal
-import zmq                          # pip install pyzmq
-import mysql                        # pip install mysql
-import mysql.connector              # pip install mysql-connector-python
-from jsonschema import validate     # pip install jsonschema
+import zmq
+import mysql
+import mysql.connector
+from dotenv import load_dotenv
+from jsonschema import validate, ValidationError
 
 # Constants
 PORT = 8080
@@ -22,7 +25,8 @@ SCHEMA = {
 parser = argparse.ArgumentParser(
     description='Micro API server to authenticate users against database.')
 parser.add_argument(
-    '-j', '--json', help='Utilize a static JSON file as the user database.', metavar='filename')
+    '-j', '--json', help='Utilize a static JSON file as the user database.',
+    metavar='filename')
 args = parser.parse_args()
 
 # Config to interrupt threaded socket listener
@@ -37,78 +41,133 @@ def signal_handler(signum, frame):
     interrupted = True
 
 
-def server_up(db_middleware, json_db=None):
+def server_up(db_middleware, db):
     """Brings up the ZMQ socket listener; behavior contingent on supplied
         middleware function.
 
     Args:
         db_middleware (func): middleware function to determine behavior on
             received message on socket
-        json_db (object): JSON db object, loaded via python JSON library
+        db (object): JSON db object loaded via python JSON library; OR mysql
+            connector object.
     """
     # ZMQ Socket Config
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:" + str(PORT))
-    print(f"\nServer listening on socket at port {PORT}...\n")
+    print(f"Server listening on socket at port {PORT}...\n")
 
     # Signal observe to allow for Ctrl-C (SIGINT) interrupt of socket listener;
     #   see http://zguide2.zeromq.org/py:interrupt
     signal.signal(signal.SIGINT, signal_handler)
     # Socket listen loop
     while True:
-        db_middleware(socket, json_db)
-        if interrupted:
-            break
+        # Listen for JSON content on socket and validate
+        try:
+            if interrupted:
+                break
+            recv_msg = socket.recv_json(zmq.NOBLOCK)
+        # When zmq.NOBLOCK flag is set above, the following exception is fired
+        #   on every socket listen tick that a message is not received. However,
+        #   this is needed to allow for SIGINT (Ctrl-C) interrupts to work.
+        #   See http://zguide2.zeromq.org/py:interrupt
+        except zmq.ZMQError:
+            continue
+        else:
+            # Once message is recieved:
+            try:
+                # Utilize JSON Schema defined in SCHEMA constant to validate
+                #   recieved JSON
+                validate(instance=recv_msg, schema=SCHEMA)
+            except ValidationError:
+                print("Logger: Invalid JSON content received.")
+                socket.send_string('invalid')
+                continue
+            else:
+                db_middleware(socket, recv_msg, db)
 
 
-def json_middleware(socket, json_db):
-    # Listen for JSON content on socket and validate
-    try:
-        json_msg = socket.recv_json(zmq.NOBLOCK)
-    # When zmq.NOBLOCK flag is set above, the following exception is fired
-    #   on every socket listen tick that a message is not received. However,
-    #   this is needed to allow for SIGINT (Ctrl-C) interrupts to work.
-    #   See http://zguide2.zeromq.org/py:interrupt
-    except zmq.ZMQError:
-        return
+def json_middleware(socket, recv_msg, json_db):
+    """Middleware to handle checking of recieved msg username and password to
+        static json file database. Sends socket message of 'valid' if received
+        username and password combo match case-sensitive entry in DB, 'invalid'
+        if not.
 
-    # Once message is recieved:
-    try:
-        # Utilize JSON Schema defined in SCHEMA constant to validate recieved JSON
-        validate(instance=json_msg, schema=SCHEMA)
-    except:
-        print("Exception: Invalid JSON content received.")
-        socket.send_string('invalid')
-        return
+    Args:
+        socket (object): open ZMQ socket
+        recv_msg (dict): validated JSON message received on listening socket
+        json_db (object): object representation of static db, imported from
+            JSON file.
+    """
+    # If JSON is valid, check static JSON DB for case-sensitive matching
+    #   username; O(n). If matching username found, check if associated
+    #   password matches; send response.
+    for user in json_db:
+        if user['username'] == recv_msg['username']:
+            if user['password'] == recv_msg['password']:
+                socket.send_string('valid')
+                return
+    socket.send_string('invalid')
+    return
+
+
+def mysql_middleware(socket, recv_msg, mysql_connector):
+    """Middleware to handle checking of received msg username and password
+        against mysql db. Sends socket message of 'valid' if received
+        username and password combo match case-sensitive entry in DB, 'invalid'
+        if not.
+
+    Args:
+        socket (object): open ZMQ socket
+        recv_msg (dict): validated JSON message received on listening socket
+        mysql_connector (object): mysql_connector object, initialized and active
+            for connected db.
+    """
+    # Note: 'BINARY' makes the query values case-sensitive
+    query = f"SELECT username, password FROM users WHERE username = BINARY '{recv_msg['username']}' AND password = BINARY '{recv_msg['password']}';"
+    cursor = mysql_connector.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    if result:
+        socket.send_string('valid')
     else:
-        # If JSON is valid, check static JSON DB for case-sensitive matching username; O(n)
-        #   If matching username found, check if associated password matches; send response.
-        for user in json_db:
-            if user['username'] == json_msg['username']:
-                if user['password'] == json_msg['password']:
-                    socket.send_string('valid')
-                    return
         socket.send_string('invalid')
-        return
+    return
 
 
 def main():
+    """Main function - parse CLI arguments if provided, detect initial errors
+        with DB connections, otherwise put server up.
+    """
     # If the -j or --json flag and a JSON db file passed to CLI call.
     if args.json:
         # Try to open and parse given JSON db file as JSON files
         try:
-            db = open(args.json)
+            db = open(args.json, encoding='UTF-8')
             json_db = json.load(db)
-        except:
-            print("Exception: provided JSON database file is invalid.")
+        except Exception as err:
+            print(
+                f"Exception: JSON DB issue. Check static DB file \n\t{str(err)}")
             quit()
         else:
             server_up(json_middleware, json_db)
-    # TODO: Default (no CLI flag) call, for SQL database implementation.
+    # Default (no CLI flags); mysql database implementation.
     else:
-        print("Not yet implemented. Placeholder for SQL database integration.\
-            \nPlease use -h argument for more options.")
+        try:
+            # attempt to load DB details from .env file in same directory
+            load_dotenv()
+            mydb = mysql.connector.connect(
+                host=os.environ.get('MYSQL_HOST'),
+                user=os.environ.get('MYSQL_USER'),
+                password=os.environ.get('MYSQL_PASSWORD'),
+                db=os.environ.get('MYSQL_DATABASE'))
+        except Exception as err:
+            print(
+                f"Exception: DB initialization/connection issue. Check env "
+                f"file, env variables, and network. \n\t{str(err)}")
+            quit()
+        else:
+            server_up(mysql_middleware, mydb)
 
 
 if __name__ == "__main__":
